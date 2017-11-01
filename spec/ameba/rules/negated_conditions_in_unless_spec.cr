67506fc6430053872486ec94273bc393c47b04f5
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-11-01 21:29:37 +0200

    New rule: negated conditions in unless

---
+++ spec/ameba/rules/negated_conditions_in_unless_spec.cr
@@ -0,0 +1,68 @@
+require "../../spec_helper"
+
+module Ameba::Rules
+  subject = NegatedConditionsInUnless.new
+
+  describe NegatedConditionsInUnless do
+    it "passes with a unless without negated condition" do
+      s = Source.new %(
+        unless a
+          :ok
+        end
+
+        :ok unless b
+
+        unless s.empty?
+          :ok
+        end
+      )
+      subject.catch(s).valid?.should be_true
+    end
+
+    it "fails if there is a negated condition in unless" do
+      s = Source.new %(
+        unless !a
+          :nok
+        end
+      )
+      subject.catch(s).valid?.should be_false
+    end
+
+    it "fails if one of AND conditions is negated" do
+      s = Source.new %(
+        unless a && !b
+          :nok
+        end
+      )
+      subject.catch(s).valid?.should be_false
+    end
+
+    it "fails if one of OR conditions is negated" do
+      s = Source.new %(
+        unless a || !b
+          :nok
+        end
+      )
+      subject.catch(s).valid?.should be_false
+    end
+
+    it "fails if one of inner conditions is negated" do
+      s = Source.new %(
+        unless a && (b || !c)
+          :nok
+        end
+      )
+      subject.catch(s).valid?.should be_false
+    end
+
+    it "reports rule, pos and message" do
+      s = Source.new ":nok unless !s.empty?"
+      subject.catch(s).valid?.should be_false
+
+      error = s.errors.first
+      error.rule.should_not be_nil
+      error.pos.should eq 1
+      error.message.should eq "Avoid negated conditions in unless blocks"
+    end
+  end
+end
