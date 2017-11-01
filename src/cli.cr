be1a1ae899c0f92b51c5cb9939c76a21d1424880
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-11-01 16:39:47 +0200

    Ameba cli & binary

---
+++ src/cli.cr
@@ -0,0 +1,18 @@
+require "option_parser"
+require "./ameba"
+
+OptionParser.parse(ARGV) do |parser|
+  parser.banner = "Usage: ameba [options]"
+
+  parser.on("-v", "--version", "Print version") do
+    puts Ameba::VERSION
+    exit 0
+  end
+
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit 0
+  end
+end
+
+Ameba.run
