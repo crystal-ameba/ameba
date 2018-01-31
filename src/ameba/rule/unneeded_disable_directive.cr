8075c39aa935e0d6352ebfe8deb823be0acf4b08
Vitalii Elenhaupt <velenhaupt@gmail.com>
2018-01-31 19:46:44 +0200

    Unneeded disable directive

---
+++ src/ameba/rule/unneeded_disable_directive.cr
@@ -0,0 +1,50 @@
+module Ameba::Rule
+  # A rule that reports unneeded disable directives.
+  # For example, this is considered invalid:
+  #
+  # ```
+  # # ameba:disable PredicateName
+  # def comment?
+  #   do_something
+  # end
+  # ```
+  #
+  # as the predicate name is correct and comment directive does not
+  # have any effect, the snippet should be written as the following:
+  #
+  # ```
+  # def comment?
+  #   do_something
+  # end
+  # ```
+  #
+  struct UnneededDisableDirective < Base
+    properties do
+      description = "Reports unneeded disable directives in comments"
+    end
+
+    def test(source)
+      Tokenizer.new(source).run do |token|
+        next unless token.type == :COMMENT
+        next unless directive = source.parse_inline_directive(token.value.to_s)
+        next unless names = unneeded_disables(source, directive, token.location)
+        next unless names.any?
+
+        source.error self, token.location,
+          "Unnecessary disabling of #{names.join(", ")}"
+      end
+    end
+
+    private def unneeded_disables(source, directive, location)
+      return unless directive[:action] == "disable"
+
+      directive[:rules].reject do |rule_name|
+        any = source.errors.any? do |error|
+          error.rule.name == rule_name &&
+            error.disabled? &&
+            error.location.try(&.line_number) == location.line_number
+        end
+      end
+    end
+  end
+end
