d6c6af883ddabb510b55297bb56c0fdcb66ce79d
Hugo Abonizio <hugo_abonizio@hotmail.com>
2018-04-12 09:52:09 -0300

    Ignores PredicateName if alternative isn't valid (e.g. is_404?)

--- src/ameba/rule/predicate_name.cr
+++ src/ameba/rule/predicate_name.cr
@@ -1,6 +1,6 @@
 module Ameba::Rule
   # A rule that disallows tautological predicate names, meaning those that
-  # start with the prefix `has_` or the prefix `is_`.
+  # start with the prefix `has_` or the prefix `is_`. Ignores if the alternative isn't valid Crystal code (e.g. `is_404?`).
   #
   # Favour these:
   #
@@ -40,8 +40,11 @@ module Ameba::Rule
 
     def test(source, node : Crystal::Def)
       if node.name =~ /^(is|has)_(\w+)\?/
+        alternative = $2
+        return unless alternative =~ /^[a-z_][a-zA-Z_0-9]*$/
+
         source.error self, node.location,
-          "Favour method name '#{$2}?' over '#{node.name}'"
+          "Favour method name '#{alternative}?' over '#{node.name}'"
       end
     end
   end
