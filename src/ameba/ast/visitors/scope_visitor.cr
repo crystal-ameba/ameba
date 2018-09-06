e6c9c51ee8a47bb1075aa271b9b5b7f38aeb247c
Vitalii Elenhaupt <velenhaupt@gmail.com>
2018-09-07 00:39:47 +0300

    Increase precision of matching the record macro

--- src/ameba/ast/visitors/scope_visitor.cr
+++ src/ameba/ast/visitors/scope_visitor.cr
@@ -3,20 +3,8 @@ require "./base_visitor"
 module Ameba::AST
   # AST Visitor that traverses the source and constructs scopes.
   class ScopeVisitor < BaseVisitor
-    SUPER = "super"
-    # List of special top level macros that use assignments
-    # as the macro arguments thus such arguments are not treated as assignments.
-    #
-    # For example:
-    #
-    # ```
-    # bar = 1
-    # record Foo, bar = 2 # this is not an assignment
-    # bar                 # => 1
-    # ```
-    SPECIAL_TOP_LEVEL_MACROS = %w(
-      record
-    )
+    SUPER_NODE_NAME  = "super"
+    RECORD_NODE_NAME = "record"
 
     @current_scope : Scope
 
@@ -157,17 +145,21 @@ module Ameba::AST
     def visit(node : Crystal::Call)
       case @current_scope
       when .def?
-        if node.name == SUPER && node.args.empty?
+        if node.name == SUPER_NODE_NAME && node.args.empty?
           @current_scope.arguments.each do |arg|
             variable = arg.variable
             variable.reference(variable.node, @current_scope).explicit = false
           end
         end
       when .top_level?
-        return false if SPECIAL_TOP_LEVEL_MACROS.includes?(node.name)
+        return false if record_macro?(node)
       end
 
       true
+    end
+
+    private def record_macro?(node)
+      node.name == RECORD_NODE_NAME && node.args.first?.is_a?(Crystal::Path)
     end
   end
 end
