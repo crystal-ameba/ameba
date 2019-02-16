1c0a33111f31f13857a0b03deb3dece07a446039
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-02-16 15:42:59 +0100

    Cover a violation supressed by increased threshold

--- spec/ameba/rule/metrics/cyclomatic_complexity_spec.cr
+++ spec/ameba/rule/metrics/cyclomatic_complexity_spec.cr
@@ -2,6 +2,20 @@ require "../../../spec_helper"
 
 module Ameba::Rule::Metrics
   subject = CyclomaticComplexity.new
+  complex_method = <<-CODE
+    def hello(a, b, c)
+      if a && b && c
+        begin
+          while true
+            return if false && b
+          end
+          ""
+        rescue
+          ""
+        end
+      end
+    end
+  CODE
 
   describe CyclomaticComplexity do
     it "passes for empty methods" do
@@ -14,20 +28,7 @@ module Ameba::Rule::Metrics
 
     it "reports one issue for a complex method" do
       subject.max_complexity = 5
-      source = Source.new %(
-        def hello(a, b, c)
-          if a && b && c
-            begin
-              while true
-                return if false && b
-              end
-              ""
-            rescue
-              ""
-            end
-          end
-        end
-      ), "source.cr"
+      source = Source.new(complex_method, "source.cr")
       subject.catch(source).should_not be_valid
 
       issue = source.issues.first
@@ -35,6 +36,12 @@ module Ameba::Rule::Metrics
       issue.location.to_s.should eq "source.cr:1:1"
       issue.end_location.to_s.should eq "source.cr:12:3"
       issue.message.should eq "Cyclomatic complexity too high [8/5]"
+    end
+
+    it "doesn't report an issue for an increased threshold" do
+      subject.max_complexity = 100
+      source = Source.new(complex_method, "source.cr")
+      subject.catch(source).should be_valid
     end
   end
 end
