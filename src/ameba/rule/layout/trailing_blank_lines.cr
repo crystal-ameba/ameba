db4c42d13da4614fdebb66de94751ff2b4479fc1
Vladislav Trotsenko <admin@bestweb.com.ua>
2020-05-02 15:52:56 +0300

    Feature/Layout trailing lines, final newline checkup
    * Added final newline check-up rule to layout trailing lines
    * Updated tests
    * Updated version

--- src/ameba/rule/layout/trailing_blank_lines.cr
+++ src/ameba/rule/layout/trailing_blank_lines.cr
@@ -13,11 +13,18 @@ module Ameba::Rule::Layout
       description "Disallows trailing blank lines"
     end
 
-    MSG = "Blank lines detected at the end of the file"
+    MSG               = "Blank lines detected at the end of the file"
+    MSG_FINAL_NEWLINE = "Final newline missing"
 
     def test(source)
-      if source.lines.size > 1 && source.lines[-2, 2].join.strip.empty?
-        issue_for({source.lines.size - 1, 1}, MSG)
+      source_lines = source.lines
+      last_source_line = source_lines.last
+      source_lines_size = source.lines.size
+      return if source_lines_size == 1 && last_source_line.empty?
+
+      last_line_not_empty = !last_source_line.strip.empty?
+      if source.lines.size >= 1 && (source_lines.last(2).join.strip.empty? || last_line_not_empty)
+        issue_for({source_lines_size - 1, 1}, last_line_not_empty ? MSG_FINAL_NEWLINE : MSG)
       end
     end
   end
