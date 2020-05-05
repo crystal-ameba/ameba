a5f3612a74935a8e0b50b16d66ddd3d580f37ce1
Vladislav Trotsenko <admin@bestweb.com.ua>
2020-05-05 15:09:02 +0300

    Final refactor

--- src/ameba/rule/layout/trailing_blank_lines.cr
+++ src/ameba/rule/layout/trailing_blank_lines.cr
@@ -18,6 +18,8 @@ module Ameba::Rule::Layout
 
     def test(source)
       source_lines = source.lines
+      return if source_lines.empty?
+
       last_source_line = source_lines.last
       source_lines_size = source_lines.size
       return if source_lines_size == 1 && last_source_line.empty?
