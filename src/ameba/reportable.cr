ded60b410967dc9f54de4dc68c36e9ceee35e244
Sijawusz Pur Rahnama <sija@sija.pl>
2021-01-22 21:41:59 +0100

    Cleanup Reportable method definitions
    There’s no need for double splats, since they mess up method resolution, and obscure the actual - single (!) - argument - `status`, so… be gone
    
    Also, all of the helpers return the constructed `Issue` like a behaving good methods.

--- src/ameba/reportable.cr
+++ src/ameba/reportable.cr
@@ -5,35 +5,37 @@ module Ameba
     getter issues = [] of Issue
 
     # Adds a new issue to the list of issues.
-    def add_issue(rule, location : Crystal::Location?, end_location : Crystal::Location?, message, status = nil)
+    def add_issue(rule, location, end_location, message, status = nil) : Issue
       status ||= :disabled if location_disabled?(location, rule)
-      issues << Issue.new rule, location, end_location, message, status
+      Issue.new(rule, location, end_location, message, status).tap do |issue|
+        issues << issue
+      end
     end
 
-    # Adds a new issue for AST *node*.
-    def add_issue(rule, node : Crystal::ASTNode, message, **args)
-      add_issue rule, node.location, node.end_location, message, **args
+    # Adds a new issue for Crystal AST *node*.
+    def add_issue(rule, node : Crystal::ASTNode, message, status = nil) : Issue
+      add_issue rule, node.location, node.end_location, message, status
     end
 
     # Adds a new issue for Crystal *token*.
-    def add_issue(rule, token : Crystal::Token, message, **args)
-      add_issue rule, token.location, nil, message, **args
+    def add_issue(rule, token : Crystal::Token, message, status = nil) : Issue
+      add_issue rule, token.location, nil, message, status
     end
 
     # Adds a new issue for *location* defined by line and column numbers.
-    def add_issue(rule, location : Tuple(Int32, Int32), message, **args)
+    def add_issue(rule, location : {Int32, Int32}, message, status = nil) : Issue
       location = Crystal::Location.new path, *location
-      add_issue rule, location, nil, message, **args
+      add_issue rule, location, nil, message, status
     end
 
     # Adds a new issue for *location* and *end_location* defined by line and column numbers.
-    def add_issue(rule, location : Tuple(Int32, Int32), end_location : Tuple(Int32, Int32), message, **args)
-      location = Crystal::Location.new path, *location
-      end_location = Crystal::Location.new path, *end_location
-      add_issue rule, location, end_location, message, **args
+    def add_issue(rule, location : {Int32, Int32}, end_location : {Int32, Int32}, message, status = nil) : Issue
+      location = Crystal::Location.new(path, *location)
+      end_location = Crystal::Location.new(path, *end_location)
+      add_issue rule, location, end_location, message, status
     end
 
-    # Returns true if the list of not disabled issues is empty, false otherwise.
+    # Returns `true` if the list of not disabled issues is empty, `false` otherwise.
     def valid?
       issues.reject(&.disabled?).empty?
     end
