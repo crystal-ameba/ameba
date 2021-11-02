703ca0b7be5807661e2282133d12d663240160d9
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-01 18:06:13 -0700

    Convert Style/IsAFilter spec

--- spec/ameba/rule/style/is_a_filter_spec.cr
+++ spec/ameba/rule/style/is_a_filter_spec.cr
@@ -5,53 +5,57 @@ module Ameba::Rule::Style
 
   describe IsAFilter do
     it "passes if there is no potential performance improvements" do
-      source = Source.new %(
+      expect_no_issues subject, %(
         [1, 2, nil].select(Int32)
         [1, 2, nil].reject(Nil)
       )
-      subject.catch(source).should be_valid
     end
 
     it "reports if there is .is_a? call within select" do
-      source = Source.new %(
+      source = expect_issue subject, %(
         [1, 2, nil].select(&.is_a?(Int32))
+                  # ^^^^^^^^^^^^^^^^^^^^^^ error: Use `select(Int32)` instead of `select {...}`
       )
-      subject.catch(source).should_not be_valid
+
+      expect_correction source, %(
+        [1, 2, nil].select(Int32)
+      )
     end
 
     it "reports if there is .nil? call within reject" do
-      source = Source.new %(
+      source = expect_issue subject, %(
         [1, 2, nil].reject(&.nil?)
+                  # ^^^^^^^^^^^^^^ error: Use `reject(Nil)` instead of `reject {...}`
       )
-      subject.catch(source).should_not be_valid
+
+      expect_correction source, %(
+        [1, 2, nil].reject(Nil)
+      )
     end
 
     it "does not report if there .is_a? call within block with multiple arguments" do
-      source = Source.new %(
+      expect_no_issues subject, %(
         t.all? { |_, v| v.is_a?(String) }
         t.all? { |foo, bar| foo.is_a?(String) }
         t.all? { |foo, bar| bar.is_a?(String) }
       )
-      subject.catch(source).should be_valid
     end
 
     context "properties" do
       it "allows to configure filter_names" do
-        source = Source.new %(
-          [1, 2, nil].reject(&.nil?)
-        )
         rule = IsAFilter.new
         rule.filter_names = %w(select)
-        rule.catch(source).should be_valid
+        expect_no_issues rule, %(
+          [1, 2, nil].reject(&.nil?)
+        )
       end
     end
 
     context "macro" do
       it "doesn't report in macro scope" do
-        source = Source.new %(
+        expect_no_issues subject, %(
           {{ [1, 2, nil].reject(&.nil?) }}
         )
-        subject.catch(source).should be_valid
       end
     end
 
