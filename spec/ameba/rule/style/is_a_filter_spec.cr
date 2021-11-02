fb39c4c43bd4aec0f2e47c6bb1f4c4186df75377
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-02 08:27:50 -0700

    Update `Style/IsAFilter` spec

--- spec/ameba/rule/style/is_a_filter_spec.cr
+++ spec/ameba/rule/style/is_a_filter_spec.cr
@@ -5,57 +5,57 @@ module Ameba::Rule::Style
 
   describe IsAFilter do
     it "passes if there is no potential performance improvements" do
-      expect_no_issues subject, %(
+      expect_no_issues subject, <<-CRYSTAL
         [1, 2, nil].select(Int32)
         [1, 2, nil].reject(Nil)
-      )
+        CRYSTAL
     end
 
     it "reports if there is .is_a? call within select" do
-      source = expect_issue subject, %(
+      source = expect_issue subject, <<-CRYSTAL
         [1, 2, nil].select(&.is_a?(Int32))
                   # ^^^^^^^^^^^^^^^^^^^^^^ error: Use `select(Int32)` instead of `select {...}`
-      )
+        CRYSTAL
 
-      expect_correction source, %(
+      expect_correction source, <<-CRYSTAL
         [1, 2, nil].select(Int32)
-      )
+        CRYSTAL
     end
 
     it "reports if there is .nil? call within reject" do
-      source = expect_issue subject, %(
+      source = expect_issue subject, <<-CRYSTAL
         [1, 2, nil].reject(&.nil?)
                   # ^^^^^^^^^^^^^^ error: Use `reject(Nil)` instead of `reject {...}`
-      )
+        CRYSTAL
 
-      expect_correction source, %(
+      expect_correction source, <<-CRYSTAL
         [1, 2, nil].reject(Nil)
-      )
+        CRYSTAL
     end
 
     it "does not report if there .is_a? call within block with multiple arguments" do
-      expect_no_issues subject, %(
+      expect_no_issues subject, <<-CRYSTAL
         t.all? { |_, v| v.is_a?(String) }
         t.all? { |foo, bar| foo.is_a?(String) }
         t.all? { |foo, bar| bar.is_a?(String) }
-      )
+        CRYSTAL
     end
 
     context "properties" do
       it "allows to configure filter_names" do
         rule = IsAFilter.new
         rule.filter_names = %w(select)
-        expect_no_issues rule, %(
+        expect_no_issues rule, <<-CRYSTAL
           [1, 2, nil].reject(&.nil?)
-        )
+          CRYSTAL
       end
     end
 
     context "macro" do
       it "doesn't report in macro scope" do
-        expect_no_issues subject, %(
+        expect_no_issues subject, <<-CRYSTAL
           {{ [1, 2, nil].reject(&.nil?) }}
-        )
+          CRYSTAL
       end
     end
 
