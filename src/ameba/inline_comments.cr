04bd5c33005a2a1a2f26ad5b4bd9cf2ac8786b32
Anton Maminov <anton.linux@gmail.com>
2022-08-01 11:22:25 +0300

    fix restriction issue
    caused by https://github.com/crystal-lang/crystal/pull/12335

--- src/ameba/inline_comments.cr
+++ src/ameba/inline_comments.cr
@@ -36,7 +36,7 @@ module Ameba
     #   Time.epoch(1483859302)
     # end
     # ```
-    def location_disabled?(location, rule)
+    def location_disabled?(location : Crystal::Location?, rule)
       return false if rule.name.in?(Rule::SPECIAL)
       return false unless line_number = location.try &.line_number.try &.- 1
       return false unless line = lines[line_number]?
