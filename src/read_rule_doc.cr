3d6125473907062567ae33ac00e1ebd60f9a478a
Sijawusz Pur Rahnama <sija@sija.pl>
2022-10-29 01:23:26 +0200

    Populate `Rule::Base+.parsed_doc` on compile time

---
+++ src/read_rule_doc.cr
@@ -0,0 +1,32 @@
+require "compiler/crystal/syntax/*"
+
+private class DocFinder < Crystal::Visitor
+  getter type_name : String?
+  getter doc : String?
+
+  def initialize(nodes, @type_name)
+    self.accept(nodes)
+  end
+
+  def visit(node : Crystal::ASTNode)
+    return false if @doc
+
+    if node.responds_to?(:name) &&
+       (name = node.name) &&
+       name.is_a?(Crystal::Path) &&
+       name.names.last? == @type_name
+      @doc = node.doc
+    end
+
+    true
+  end
+end
+
+type_name, path_to_source_file = ARGV
+
+source = File.read(path_to_source_file)
+nodes = Crystal::Parser.new(source)
+  .tap(&.wants_doc = true)
+  .parse
+
+puts DocFinder.new(nodes, type_name).doc
