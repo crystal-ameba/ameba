5c08b64e72798b4e8e44f5c8e4bd156df212c809
Sijawusz Pur Rahnama <sija@sija.pl>
2022-11-15 01:26:46 +0100

    Add `Lint/LiteralAssignmentsInExpressions` rule

---
+++ src/ameba/rule/lint/literal_assignments_in_expressions.cr
@@ -0,0 +1,43 @@
+module Ameba::Rule::Lint
+  # A rule that disallows assignments with literal values
+  # in control expressions.
+  #
+  # For example, this is considered invalid:
+  #
+  # ```
+  # if foo = 42
+  #   do_something
+  # end
+  # ```
+  #
+  # And most likely should be replaced by the following:
+  #
+  # ```
+  # if foo == 42
+  #   do_something
+  # end
+  # ```
+  #
+  # YAML configuration example:
+  #
+  # ```
+  # Lint/LiteralAssignmentsInExpressions:
+  #   Enabled: true
+  # ```
+  class LiteralAssignmentsInExpressions < Base
+    include AST::Util
+
+    properties do
+      description "Disallows assignments with literal values in control expressions"
+    end
+
+    MSG = "Detected assignment with a literal value in control expression"
+
+    def test(source, node : Crystal::If | Crystal::Unless | Crystal::Case | Crystal::While | Crystal::Until)
+      return unless (cond = node.cond).is_a?(Crystal::Assign)
+      return unless literal?(cond.value, include_paths: true)
+
+      issue_for cond, MSG
+    end
+  end
+end
