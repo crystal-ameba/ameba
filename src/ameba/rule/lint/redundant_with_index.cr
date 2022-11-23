1399aa3cdfe907637d2e9253e75a932f28615f3a
Sijawusz Pur Rahnama <sija@sija.pl>
2022-11-23 16:15:46 +0100

    Doc fixes

--- src/ameba/rule/lint/redundant_with_index.cr
+++ src/ameba/rule/lint/redundant_with_index.cr
@@ -2,6 +2,7 @@ module Ameba::Rule::Lint
   # A rule that disallows redundant `with_index` calls.
   #
   # For example, this is considered invalid:
+  #
   # ```
   # collection.each.with_index do |e|
   #   # ...
