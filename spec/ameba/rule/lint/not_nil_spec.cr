2fb453c61f9e827da27fad7324b3a7365f2fc7cb
Sijawusz Pur Rahnama <sija@sija.pl>
2022-12-08 14:07:10 +0100

    Add additional spec for `Lint/NotNil` rule

--- spec/ameba/rule/lint/not_nil_spec.cr
+++ spec/ameba/rule/lint/not_nil_spec.cr
@@ -18,6 +18,13 @@ module Ameba::Rule::Lint
         CRYSTAL
     end
 
+    it "reports if there is a `not_nil!` call in the middle of the call-chain" do
+      expect_issue subject, <<-CRYSTAL
+        (1..3).first?.not_nil!.to_s
+                    # ^^^^^^^^ error: Avoid using `not_nil!`
+        CRYSTAL
+    end
+
     context "macro" do
       it "doesn't report in macro scope" do
         expect_no_issues subject, <<-CRYSTAL
