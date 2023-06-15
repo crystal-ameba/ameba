0140fd3573f6345d88bdf39eb5eadbbdcf771081
Sijawusz Pur Rahnama <sija@sija.pl>
2023-06-15 07:28:11 +0200

    Add `Lint/Typos` rule

---
+++ spec/ameba/rule/lint/typos_spec.cr
@@ -0,0 +1,32 @@
+require "../../../spec_helper"
+
+private def check_typos_bin!
+  unless Ameba::Rule::Lint::Typos::BIN_PATH
+    pending! "`typos` executable is not available"
+  end
+end
+
+module Ameba::Rule::Lint
+  subject = Typos.new
+    .tap(&.fail_on_error = true)
+
+  describe Typos do
+    it "reports typos" do
+      check_typos_bin!
+
+      source = expect_issue subject, <<-CRYSTAL
+        # method with no arugments
+                       # ^^^^^^^^^ error: Typo found: arugments -> arguments
+        def tpos
+          # ^^^^ error: Typo found: tpos -> typos
+        end
+        CRYSTAL
+
+      expect_correction source, <<-CRYSTAL
+        # method with no arguments
+        def typos
+        end
+        CRYSTAL
+    end
+  end
+end
