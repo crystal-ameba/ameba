1b661d633d077eb317fc8f0cac1d3ed0e2a72ba6
Sijawusz Pur Rahnama <sija@sija.pl>
2023-12-27 23:47:58 +0100

    Refactor `ScopeVisitor` to ignore accessor macros

--- src/ameba/ast/scope.cr
+++ src/ameba/ast/scope.cr
@@ -186,6 +186,16 @@ module Ameba::AST
       node.is_a?(Crystal::Def)
     end
 
+    # Returns `true` if current scope is a class, `false` otherwise.
+    def class_def?
+      node.is_a?(Crystal::ClassDef)
+    end
+
+    # Returns `true` if current scope is a module, `false` otherwise.
+    def module_def?
+      node.is_a?(Crystal::ModuleDef)
+    end
+
     # Returns `true` if this scope is a top level scope, `false` otherwise.
     def top_level?
       outer_scope.nil? || type_definition?
