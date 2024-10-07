d497e3228e168a5426adae41ded8d5f50e6ab8ad
Sijawusz Pur Rahnama <sija@sija.pl>
2024-10-07 23:42:16 +0200

    Extract rule selection logic into its own helper method

--- src/ameba/runner.cr
+++ src/ameba/runner.cr
@@ -74,13 +74,15 @@ module Ameba
 
     protected def initialize(rules, @sources, @formatter, @severity, @autocorrect = false, @version = nil)
       @rules =
-        rules.select do |rule|
-          rule.enabled? && !rule.special? &&
-            (!(version = @version) || !(since_version = rule.since_version) ||
-              since_version <= version)
-        end
+        rules.select { |rule| rule_runnable?(rule) }
       @unneeded_disable_directive_rule =
         rules.find &.class.==(Rule::Lint::UnneededDisableDirective)
+    end
+
+    protected def rule_runnable?(rule)
+      rule.enabled? && !rule.special? &&
+        (!(version = @version) || !(since_version = rule.since_version) ||
+          since_version <= version)
     end
 
     # Performs the inspection. Iterates through all sources and test it using
