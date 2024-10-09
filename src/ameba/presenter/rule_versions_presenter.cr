f0fcc699a2dec9788d650e17cedd767ec79b403b
Sijawusz Pur Rahnama <sija@sija.pl>
2024-10-10 01:41:03 +0200

    Make `RuleVersionsPresenter` list all rules grouped by version

--- src/ameba/presenter/rule_versions_presenter.cr
+++ src/ameba/presenter/rule_versions_presenter.cr
@@ -1,11 +1,31 @@
 module Ameba::Presenter
   class RuleVersionsPresenter < BasePresenter
-    def run(rules)
-      versions =
-        rules.compact_map(&.since_version).sort!.uniq!
+    def run(rules, verbose = true)
+      missing_version = SemanticVersion.new(0, 0, 0)
+      versions = rules
+        .sort_by { |rule| rule.since_version || missing_version }
+        .group_by(&.since_version)
 
-      versions.each do |version|
-        output.puts "- %s" % version.to_s.colorize(:green)
+      first = true
+
+      versions.each do |version, version_rules|
+        if verbose
+          output.puts unless first
+          if version
+            output.puts "- %s" % version.to_s.colorize(:green)
+          else
+            output.puts "- %s" % "N/A".colorize(:dark_gray)
+          end
+          version_rules.map(&.name).sort!.each do |name|
+            output.puts "  - %s" % name.colorize(:dark_gray)
+          end
+        else
+          if version
+            output.puts "- %s" % version.to_s.colorize(:green)
+          end
+        end
+
+        first = false
       end
     end
   end
