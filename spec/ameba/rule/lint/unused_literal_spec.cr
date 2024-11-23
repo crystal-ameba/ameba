72c82f49b0c11f1ff0f86c8ace94a18e665b94b7
Margret Riegert <margret@eowyn.net>
2024-11-23 00:40:17 -0800

    Better UnusedLiteral specs and docs

--- spec/ameba/rule/lint/unused_literal_spec.cr
+++ spec/ameba/rule/lint/unused_literal_spec.cr
@@ -4,43 +4,228 @@ module Ameba::Rule::Lint
   subject = UnusedLiteral.new
 
   describe UnusedLiteral do
-    it "passes if literals are used" do
+    it "passes if literals are used to assign" do
       expect_no_issues subject, <<-CRYSTAL
-        a = 1 == "1"
-        b = begin
-          2 == "3"
+        a = 1
+        b = "string"
+        c = begin
+          :symbol
         end
 
-        if c == b
-          puts "meow"
-        end
+        d = 1
+        d ||= {here: 1, there: 4}
 
-        def test
-          1 == 2
+        e = [10_f32, 20_f32, 30_f32]
+
+        f = -> { puts }
+        CRYSTAL
+    end
+
+    it "passes if literals implicit return in method" do
+      expect_no_issues subject, <<-CRYSTAL
+        def hello
+          if true
+            :string
+          else
+            "symbol"
+          end
         end
         CRYSTAL
     end
 
-    it "fails if a literal is not used" do
-      expect_issue subject, <<-CRYSTAL
-          :hello
-        # ^^^^^^ error: Literal value is not used
+    it "passes if unused literals are beyond return" do
+      expect_no_issues subject, <<-CRYSTAL
+        def hello : Nil
+          return :meow
 
-          "world"
-        # ^^^^^^^ error: Literal value is not used
-
-          { my: :name }
-        # ^^^^^^^^^^^^^ error: Literal value is not used
-
-          [
-        # ^ error: Literal value is not used
-            "is",
-            :a
-          ]
-
-          1234
-        # ^^^^ error: Literal value is not used
+          if true
+            :string
+          else
+            "symbol"
+          end
+        end
         CRYSTAL
+    end
+
+    it "fails if literals are top-level" do
+      expect_issue subject, <<-CRYSTAL
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp string"
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+      CRYSTAL
+    end
+
+    it "fails if literals are in def body with Nil return" do
+      expect_issue subject, <<-CRYSTAL
+      def hello : Nil
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp string"
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals are in proc body with Nil return, alongside the proc itself" do
+      expect_issue subject, <<-CRYSTAL
+      -> : Nil do
+      # ^^^^^^^^^ error: Literal value is not used
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp string"
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals in unused if" do
+      expect_issue subject, <<-CRYSTAL
+      if true
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp string"
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals are unused in case" do
+      expect_issue subject, <<-CRYSTAL
+        thing = case object
+                when Type1
+                  true
+                # ^^^^ error: Literal value is not used
+                  "meow"
+                else
+                  if begin
+                      "huh?"
+                    # ^^^^^^ error: Literal value is not used
+                      1 > 0
+                    end
+                    1234_f32
+                  # ^^^^^^^^ error: Literal value is not used
+                  end
+
+                  "woof"
+                end
+        CRYSTAL
+    end
+
+    it "fails if literals are unused in class bodies" do
+      expect_issue subject, <<-CRYSTAL
+        class MyClass
+          case object
+          when Type1
+            true
+          # ^^^^ error: Literal value is not used
+            "meow"
+          # ^^^^^^ error: Literal value is not used
+          else
+            if begin
+                "huh?"
+              # ^^^^^^ error: Literal value is not used
+                1 > 0
+              end
+              1234_f32
+            # ^^^^^^^^ error: Literal value is not used
+            end
+
+            "woof"
+          # ^^^^^^ error: Literal value is not used
+          end
+        end
+        CRYSTAL
+    end
+
+    it "fails if unused literals in rescue/ensure/else block" do
+      expect_issue subject, <<-CRYSTAL
+      a = begin
+            1234
+          # ^^^^ error: Literal value is not used
+            1234_f32
+          rescue ASDF
+            "hello world"
+          # ^^^^^^^^^^^^^ error: Literal value is not used
+            "interp string"
+          rescue QWERTY
+            [1, 2, 3, 4, 5]
+          # ^^^^^^^^^^^^^^^ error: Literal value is not used
+            {"hello" => "world"}
+          else
+            '\t'
+          # ^^^ error: Literal value is not used
+            1..2
+          ensure
+            {goodnight: moon}
+          # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+            {1, 2, 3}
+          end
+      CRYSTAL
     end
   end
 end
--- src/ameba/rule/lint/unused_literal.cr
+++ src/ameba/rule/lint/unused_literal.cr
@@ -1,4 +1,50 @@
 module Ameba::Rule::Lint
+  # A rule that disallows unused literal values (strings, symbols, integers, etc).
+  #
+  # For example, these are considered invalid:
+  #
+  # ```
+  # 1234_f32
+  #
+  # "hello world"
+  #
+  # if check?
+  #   true
+  # else
+  #   false
+  # end
+  #
+  # def method
+  #   if guard?
+  #     false
+  #   end
+  #
+  #   true
+  # end
+  # ```
+  #
+  # And these are considered valid:
+  #
+  # ```
+  # a = 1234_f32
+  #
+  # def method
+  #   if guard?
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
+  #
+  # my_proc = -> : Bool { true }
+  # ```
+  #
+  # YAML configuration example:
+  #
+  # ```
+  # Lint/UnusedLiteral:
+  #   Enabled: true
+  # ```
   class UnusedLiteral < Base
     properties do
       description "Disallows unused literal values"
@@ -14,7 +60,7 @@ module Ameba::Rule::Lint
       source,
       node : Crystal::BoolLiteral | Crystal::CharLiteral | Crystal::HashLiteral |
              Crystal::ProcLiteral | Crystal::ArrayLiteral | Crystal::RangeLiteral |
-             Crystal::RegexLiteral | Crystal::TupleLiteral | Crystal::NumberLiteral |
+             Crystal::TupleLiteral | Crystal::NumberLiteral |
              Crystal::StringLiteral | Crystal::SymbolLiteral |
              Crystal::NamedTupleLiteral,
       last_is_used : Bool
