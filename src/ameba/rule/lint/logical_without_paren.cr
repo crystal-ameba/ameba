7e6d53fedcaec51fe467257a2acd1b1903817613
Margret Riegert <m.a.riegert@snet.net>
2024-11-24 15:37:13 -0800

    Update logical_without_paren.cr
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/ameba/rule/lint/logical_without_paren.cr
+++ src/ameba/rule/lint/logical_without_paren.cr
@@ -1,5 +1,7 @@
 module Ameba::Rule::Lint
-  # A rule that disallows logical operators in method args without parenthesis.
+  # A rule that disallows method calls with at least one argument, where no
+  # parentheses are used around the argument list, and a logical operator
+  # (`&&` or `||`) is used within the argument list.
   #
   # For example, this is considered invalid:
   #
