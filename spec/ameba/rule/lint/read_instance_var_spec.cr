e5b253b436463ef9218b44af21ef9924f75516ce
Margret Riegert <margret@eowyn.net>
2024-11-23 18:20:29 -0800

    Improvements to Lint/ReadInstanceVar
    Only case where this is allowed is when reading from a variable typed the same as the current class (or self).

--- spec/ameba/rule/lint/read_instance_var_spec.cr
+++ spec/ameba/rule/lint/read_instance_var_spec.cr
@@ -4,10 +4,56 @@ module Ameba::Rule::Lint
   describe ReadInstanceVar do
     subject = ReadInstanceVar.new
 
-    it "fails if an instance var is read externally" do
+    it "passes if an instance var is read from a var of the same type of the current class" do
+      expect_no_issues subject, <<-CRYSTAL
+        class MyClass
+          def test(other : MyClass)
+            @instance_var <=> other.@instance_var
+          end
+        end
+        CRYSTAL
+    end
+
+    it "passes if an instance var is read from a var typed as `self`" do
+      expect_no_issues subject, <<-CRYSTAL
+        class MyClass
+          def test(other : self)
+            @instance_var <=> other.@instance_var
+          end
+        end
+        CRYSTAL
+    end
+
+    it "fails if an instance var is read from an untyped var in a class" do
       expect_issue subject, <<-CRYSTAL
+        class MyClass
+          def test(other)
+            @instance_var <=> other.@instance_var
+                            # ^^^^^^^^^^^^^^^^^^^ error: Reading instance variables externally is not allowed.
+          end
+        end
+        CRYSTAL
+    end
+
+    it "fails if an instance var is read externally top-level" do
+      expect_issue subject, <<-CRYSTAL
+        a = MyClass.new
         a.@instance_var
         # ^^^^^^^^^^^^^ error: Reading instance variables externally is not allowed.
+        CRYSTAL
+    end
+
+    it "fails reading an instance var externally from a different ivar" do
+      expect_issue subject, <<-CRYSTAL
+        class MyClass
+          @a.@instance_var
+        # ^^^^^^^^^^^^^^^^ error: Reading instance variables externally is not allowed.
+
+          def method
+            OtherClass.new(@a.@instance_var)
+                         # ^^^^^^^^^^^^^^^^ error: Reading instance variables externally is not allowed.
+          end
+        end
         CRYSTAL
     end
   end
--- src/ameba/rule/lint/read_instance_var.cr
+++ src/ameba/rule/lint/read_instance_var.cr
@@ -2,12 +2,33 @@ module Ameba::Rule::Lint
   # A rule that disallows reading instance variables externally from an object
   # via the `object.@ivar` syntax.
   #
+  # This is only allowed when inside a method def in a class, and the object
+  # is typed to be the same as the current class.
+  #
   # For example, this is not allowed:
+  #
+  # ```
+  # class Greeter
+  #   def combine(other)
+  #     @ivar <=> other.@ivar
+  #   end
+  #
+  #   def split(other : Smiler)
+  #     @ivar - other.@ivar
+  #   end
+  # end
+  # ```
+  #
+  # And this is allowed:
   #
   # ```
   # class Greeter
   #   def combine(other : self)
   #     @ivar <=> other.@ivar
+  #   end
+  #
+  #   def split(other : Greeter)
+  #     @ivar - other.@ivar
   #   end
   # end
   # ```
@@ -21,13 +42,65 @@ module Ameba::Rule::Lint
   class ReadInstanceVar < Base
     properties do
       description "Disallows external reading of instance vars"
-      enabled false
     end
 
     MSG = "Reading instance variables externally is not allowed."
 
-    def test(source, node : Crystal::ReadInstanceVar)
-      issue_for node, MSG
+    def test(source)
+      InstanceVarReadVisitor.new(self, source)
+    end
+
+    def test(
+      source, node : Crystal::ReadInstanceVar,
+      allowed_names : Array(String)
+    ) : Nil
+      case obj = node.obj
+      when Crystal::Var
+        unless obj.name.in?(allowed_names)
+          issue_for node, MSG, prefer_name_location: true
+        end
+      else
+        issue_for node, MSG, prefer_name_location: true
+      end
+    end
+
+    private class InstanceVarReadVisitor < AST::NodeVisitor
+      @class_name : String?
+      @param_names : Array(String) = [] of String
+
+      def visit(node : Crystal::ClassDef) : Nil
+        prev_class = @class_name
+        @class_name = node.name.names[-1]?
+
+        node.body.accept(self)
+
+        @class_name = prev_class
+      end
+
+      def visit(node : Crystal::Def)
+        node.args.each do |arg|
+          case restriction = arg.restriction
+          when Crystal::Path
+            if restriction.names[-1]? == @class_name
+              @param_names << arg.name
+            end
+          when Crystal::Self
+            @param_names << arg.name
+          end
+        end
+
+        super
+      end
+
+      def end_visit(node : Crystal::Def)
+        @param_names.clear
+        super
+      end
+
+      def visit(node : Crystal::ReadInstanceVar)
+        @rule.test(@source, node, @param_names)
+        super
+      end
     end
   end
 end
