2c10363de396d421894d5fdbde9f3ba1b7ca8b89
Margret Riegert <margret@eowyn.net>
2024-12-01 23:10:24 -0500

    Add `Typing/MethodReturnTypeRestriction`

---
+++ spec/ameba/rule/typing/method_return_type_restriction_spec.cr
@@ -0,0 +1,117 @@
+require "../../../spec_helper"
+
+module Ameba::Rule::Typing
+  subject = MethodReturnTypeRestriction.new
+  subject.private_methods = true
+  subject.protected_methods = true
+  subject.undocumented = true
+
+  it "passes if a method has a return type" do
+    expect_no_issues subject, <<-CRYSTAL
+      def hello : String
+        "hello world"
+      end
+      CRYSTAL
+  end
+
+  it "fails if a method doesn't have a return type" do
+    expect_issue subject, <<-CRYSTAL
+      def hello
+        # ^^^^^ error: Methods should have a return type restriction
+        "hello world"
+      end
+      CRYSTAL
+  end
+
+  it "fails if a private method doesn't have a return type" do
+    expect_issue subject, <<-CRYSTAL
+      class Greeter
+        private def hello
+                  # ^^^^^ error: Methods should have a return type restriction
+          "hello world"
+        end
+      end
+      CRYSTAL
+  end
+
+  it "fails if a protected method doesn't have a return type" do
+    expect_issue subject, <<-CRYSTAL
+      class Greeter
+        protected def hello
+                    # ^^^^^ error: Methods should have a return type restriction
+          "hello world"
+        end
+      end
+      CRYSTAL
+  end
+
+  it "fails if a documented method doesn't have a return type" do
+    expect_issue subject, <<-CRYSTAL
+      # This is documentation about `hello`
+      def hello(a)
+        # ^^^^^ error: Methods should have a return type restriction
+        "hello world" + a
+      end
+      CRYSTAL
+  end
+
+  context "properties" do
+    context "#private_methods" do
+      it "allows relaxing restriction requirement for private methods" do
+        rule = MethodReturnTypeRestriction.new
+        rule.undocumented = true
+        rule.private_methods = false
+
+        expect_no_issues rule, <<-CRYSTAL
+          class Greeter
+            private def hello
+              "hello world"
+            end
+          end
+          CRYSTAL
+      end
+    end
+
+    context "#protected_methods" do
+      it "allows relaxing restriction requirement for protected methods" do
+        rule = MethodReturnTypeRestriction.new
+        rule.undocumented = true
+        rule.protected_methods = false
+
+        expect_no_issues rule, <<-CRYSTAL
+          class Greeter
+            protected def hello
+              "hello world"
+            end
+          end
+          CRYSTAL
+      end
+    end
+
+    context "#undocumented" do
+      rule = MethodReturnTypeRestriction.new
+      rule.undocumented = false
+
+      it "allows relaxing restriction requirement for undocumented methods" do
+        expect_no_issues rule, <<-CRYSTAL
+          class Greeter
+            def hello
+              "hello world"
+            end
+          end
+          CRYSTAL
+      end
+
+      it "allows relaxing restriction requirement for methods with a :nodoc: directive" do
+        expect_no_issues rule, <<-CRYSTAL
+          class Greeter
+            # :nodoc:
+            def hello
+              "hello world"
+            end
+          end
+          CRYSTAL
+      end
+    end
+  end
+end
---
+++ src/ameba/rule/typing/method_return_type_restriction.cr
@@ -0,0 +1,66 @@
+module Ameba::Rule::Typing
+  # A rule that enforces method definitions have a return type restriction.
+  #
+  # For example, these are considered valid:
+  #
+  # ```
+  # def hello : String
+  #   "hello world"
+  # end
+  #
+  # def listen(a, b) : Int32
+  #   0
+  # end
+  # ```
+  #
+  # And these are considered invalid:
+  #
+  # ```
+  # def hello
+  #   "hello world"
+  # end
+  #
+  # def listen(a, b)
+  #   0
+  # end
+  # ```
+  #
+  # When the config options `PrivateMethods` and `ProtectedMethods`
+  # are true, this rule is also applied to private and protected methods, respectively.
+  #
+  # The config option `Undocumented` controls whether this rule applies to undocumented methods and methods with a `:nodoc:` directive.
+  #
+  # YAML configuration example:
+  #
+  # ```
+  # Typing/MethodReturnTypeRestriction:
+  #   Enabled: false
+  #   Undocumented: false
+  #   PrivateMethods: false
+  #   ProtectedMethods: false
+  # ```
+  class MethodReturnTypeRestriction < Base
+    properties do
+      description "Recommends that methods have a return type restriction"
+      enabled false
+      undocumented false
+      private_methods false
+      protected_methods false
+    end
+
+    MSG = "Methods should have a return type restriction"
+
+    def test(source, node : Crystal::Def)
+      return if node.return_type || check_config(node)
+
+      issue_for node, MSG, prefer_name_location: true
+    end
+
+    def check_config(node : Crystal::ASTNode) : Bool
+      (!private_methods? && node.visibility.private?) ||
+        (!protected_methods? && node.visibility.protected?) ||
+        (!undocumented? && (node.doc.nil? || node.doc.try(&.starts_with?(":nodoc:")))) ||
+        false
+    end
+  end
+end
