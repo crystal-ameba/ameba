5b3c2cfcf2b9c720fb742913d50bdab49f9933ea
Margret Riegert <margret@eowyn.net>
2024-12-07 12:46:12 -0500

    Add `Lint/UnusedComparison` and `Lint/UnusedLiteral` (#507)
    * Basic implementation of Lint/UselessComparison (#235)
    
    * More comprehensive useless comparison rule using custom visitor
    
    Need to write more specs to ensure functionality, but from initial testing this works really well
    
    * Fix case statement implicit obj comparisons
    
    * UselessComparison -> UnusedComparison
    Working implementation of Lint/UnusedLiteral
    Moved ImplicitReturnVisitor to its own class and expanded it
    
    * Better UnusedLiteral specs and docs
    
    * Remove accidental hello
    
    * Add "since_version"
    
    * Apply suggestions from code review
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * visit methods return bool, add StringInterpolation support
    
    * Apply suggestions from code review
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    * Add support for Regex literals to Lint/UnusedLiteral gated by Crystal 1.15
    
    * test methods shouldnt return bool
    RegexLiteral values should be counted as used
    Add spec ensuring unused regex literals aren't counted for Crystal < 1.15
    
    ---------
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

---
+++ spec/ameba/rule/lint/unused_literal_spec.cr
@@ -0,0 +1,279 @@
+require "../../../spec_helper"
+
+module Ameba::Rule::Lint
+  subject = UnusedLiteral.new
+
+  describe UnusedLiteral do
+    it "passes if literals are used to assign" do
+      expect_no_issues subject, <<-CRYSTAL
+        a = 1
+        b = "string"
+        g = "interp \#{string}"
+        h = <<-HEREDOC
+          this is a heredoc
+          HEREDOC
+
+        c = begin
+          :symbol
+        end
+
+        d = 1
+        d ||= {here: 1, there: 4}
+
+        e = [10_f32, 20_f32, 30_f32]
+
+        f = -> { puts }
+        CRYSTAL
+    end
+
+    it "passes if literals implicit return in method" do
+      expect_no_issues subject, <<-CRYSTAL
+        def hello
+          if true
+            :string
+          else
+            "symbol"
+          end
+        end
+        CRYSTAL
+    end
+
+    it "passes if unused literals are beyond return" do
+      expect_no_issues subject, <<-CRYSTAL
+        def hello : Nil
+          return :meow
+
+          if true
+            :string
+          else
+            "symbol"
+          end
+        end
+        CRYSTAL
+    end
+
+    it "fails if literals are top-level" do
+      expect_issue subject, <<-CRYSTAL
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp \#{string}"
+      # ^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+        <<-HEREDOC
+      # ^^^^^^^^^^ error: Literal value is not used
+          this is a heredoc
+          HEREDOC
+      CRYSTAL
+    end
+
+    it "fails if literals are in def body with Nil return" do
+      expect_issue subject, <<-CRYSTAL
+      def hello : Nil
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp \#{string}"
+      # ^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+        <<-HEREDOC
+      # ^^^^^^^^^^ error: Literal value is not used
+          this is a heredoc
+          HEREDOC
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals are in proc body with Nil return, alongside the proc itself" do
+      expect_issue subject, <<-CRYSTAL
+      -> : Nil do
+      # ^^^^^^^^^ error: Literal value is not used
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp \#{string}"
+      # ^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+        <<-HEREDOC
+      # ^^^^^^^^^^ error: Literal value is not used
+          this is a heredoc
+          HEREDOC
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals in unused if" do
+      expect_issue subject, <<-CRYSTAL
+      if true
+        1234
+      # ^^^^ error: Literal value is not used
+        1234_f32
+      # ^^^^^^^^ error: Literal value is not used
+        "hello world"
+      # ^^^^^^^^^^^^^ error: Literal value is not used
+        "interp \#{string}"
+      # ^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        [1, 2, 3, 4, 5]
+      # ^^^^^^^^^^^^^^^ error: Literal value is not used
+        {"hello" => "world"}
+      # ^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        '\t'
+      # ^^^ error: Literal value is not used
+        1..2
+      # ^^^^ error: Literal value is not used
+        {goodnight: moon}
+      # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+        {1, 2, 3}
+      # ^^^^^^^^^ error: Literal value is not used
+        <<-HEREDOC
+      # ^^^^^^^^^^ error: Literal value is not used
+          this is a heredoc
+          HEREDOC
+      end
+      CRYSTAL
+    end
+
+    it "fails if literals are unused in case" do
+      expect_issue subject, <<-CRYSTAL
+        thing = case object
+                when Type1
+                  true
+                # ^^^^ error: Literal value is not used
+                  "meow"
+                else
+                  if begin
+                      "huh?"
+                    # ^^^^^^ error: Literal value is not used
+                      1 > 0
+                    end
+                    1234_f32
+                  # ^^^^^^^^ error: Literal value is not used
+                  end
+
+                  "woof"
+                end
+        CRYSTAL
+    end
+
+    it "fails if literals are unused in class bodies" do
+      expect_issue subject, <<-CRYSTAL
+        class MyClass
+          case object
+          when Type1
+            true
+          # ^^^^ error: Literal value is not used
+            "meow"
+          # ^^^^^^ error: Literal value is not used
+          else
+            if begin
+                "huh?"
+              # ^^^^^^ error: Literal value is not used
+                1 > 0
+              end
+              1234_f32
+            # ^^^^^^^^ error: Literal value is not used
+            end
+
+            "woof"
+          # ^^^^^^ error: Literal value is not used
+          end
+        end
+        CRYSTAL
+    end
+
+    it "fails if unused literals in rescue/ensure/else block" do
+      expect_issue subject, <<-CRYSTAL
+      a = begin
+            1234
+          # ^^^^ error: Literal value is not used
+            1234_f32
+          rescue ASDF
+            "hello world"
+          # ^^^^^^^^^^^^^ error: Literal value is not used
+            "interp \#{string}"
+          rescue QWERTY
+            [1, 2, 3, 4, 5]
+          # ^^^^^^^^^^^^^^^ error: Literal value is not used
+            {"hello" => "world"}
+          else
+            '\t'
+          # ^^^ error: Literal value is not used
+            <<-HEREDOC
+          # ^^^^^^^^^^ error: Literal value is not used
+              this is a heredoc
+              HEREDOC
+            1..2
+          ensure
+            {goodnight: moon}
+          # ^^^^^^^^^^^^^^^^^ error: Literal value is not used
+            {1, 2, 3}
+          end
+      CRYSTAL
+    end
+
+    # Locations for Regex literals were added in Crystal v1.15.0
+    {% if compare_versions(Crystal::VERSION, "1.15.0") >= 0 %}
+      it "fails if a regex literal is unused" do
+        expect_issue subject, <<-CRYSTAL
+            a = /hello world/
+            /goodnight moon/
+          # ^^^^^^^^^^^^^^^^ error: Literal value is not used
+            b = /goodnight moon, \#{a}/
+            /goodnight moon, \#{a}/
+          # ^^^^^^^^^^^^^^^^^^^^^^ error: Literal value is not used
+          CRYSTAL
+      end
+    {% else %}
+      it "passes if a regex literal is unused" do
+        expect_no_issues subject, <<-CRYSTAL
+              a = /hello world/
+              /goodnight moon/
+              b = /goodnight moon, \#{a}/
+              /goodnight moon, \#{a}/
+            CRYSTAL
+      end
+    {% end %}
+  end
+end
---
+++ src/ameba/ast/visitors/implicit_return_visitor.cr
@@ -0,0 +1,274 @@
+module Ameba::AST
+  class ImplicitReturnVisitor < BaseVisitor
+    # When greater than zero, indicates the current node's return value is used
+    @stack : Int32 = 0
+
+    def visit(node : Crystal::Expressions) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      last_idx = node.expressions.size - 1
+
+      swap_stack do |old_stack|
+        node.expressions.each_with_index do |exp, idx|
+          if exp.is_a?(Crystal::ControlExpression)
+            incr_stack { exp.accept(self) }
+            break
+          elsif idx == last_idx && old_stack > 0
+            incr_stack { exp.accept(self) }
+          else
+            exp.accept(self)
+          end
+        end
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::Call) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      if node.block
+        incr_stack { node.obj.try &.accept(self) }
+      else
+        node.obj.try &.accept(self)
+      end
+
+      incr_stack do
+        node.args.each &.accept(self)
+        node.named_args.try &.each &.accept(self)
+        node.block_arg.try &.accept(self)
+        node.block.try &.accept(self)
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::Arg) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.default_value.try &.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::EnumDef) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.members.each &.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::Assign | Crystal::OpAssign) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.value.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::MultiAssign) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.targets.each &.accept(self)
+      incr_stack { node.values.each &.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::If | Crystal::Unless) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.cond.accept(self) }
+      node.then.accept(self)
+      node.else.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::While | Crystal::Until) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.cond.accept(self) }
+      node.body.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::Def) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack do
+        node.args.each &.accept(self)
+        node.double_splat.try &.accept(self)
+        node.block_arg.try &.accept(self)
+      end
+
+      if (return_type = node.return_type).is_a?(Crystal::Path)
+        # Special case of the return type being nil, meaning the last
+        # line of the method body is ignored
+        if return_type.names.join("::").in?("::Nil", "Nil")
+          node.body.accept(self)
+        else
+          incr_stack { node.body.accept(self) }
+        end
+      else
+        incr_stack { node.body.accept(self) }
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::Annotation) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack do
+        node.args.each &.accept(self)
+        node.named_args.try &.each &.accept(self)
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::TypeDeclaration) : Bool
+      incr_stack { node.value.try &.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::Macro | Crystal::MacroIf | Crystal::MacroFor) : Bool
+      false
+    end
+
+    def visit(node : Crystal::UninitializedVar) : Bool
+      false
+    end
+
+    def visit(node : Crystal::ArrayLiteral | Crystal::TupleLiteral) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.elements.each &.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::StringInterpolation) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.expressions.each do |exp|
+        incr_stack { exp.accept(self) }
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::HashLiteral | Crystal::NamedTupleLiteral) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.entries.each &.value.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::Case) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.cond.try &.accept(self) }
+      node.whens.each &.accept(self)
+      node.else.try &.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::Select) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.whens.each &.accept(self)
+      node.else.try &.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::When) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.conds.each &.accept(self) }
+      node.body.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::Rescue) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.body.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::ExceptionHandler) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      node.body.accept(self)
+      node.rescues.try &.each &.accept(self)
+      node.else.try &.accept(self)
+      node.ensure.try &.accept(self)
+
+      false
+    end
+
+    def visit(node : Crystal::ControlExpression) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      incr_stack { node.exp.try &.accept(self) }
+
+      false
+    end
+
+    def visit(node : Crystal::RangeLiteral) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      unless node.from.is_a?(Crystal::NumberLiteral)
+        node.from.accept(self)
+      end
+
+      unless node.to.is_a?(Crystal::NumberLiteral)
+        node.to.accept(self)
+      end
+
+      false
+    end
+
+    def visit(node : Crystal::RegexLiteral) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      # Regex literals either contain string literals or string interpolations,
+      # both of which are "captured" by the parent regex literal
+      incr_stack { node.value.accept(self) }
+
+      false
+    end
+
+    def visit(
+      node : Crystal::BoolLiteral | Crystal::CharLiteral | Crystal::NumberLiteral |
+             Crystal::StringLiteral | Crystal::SymbolLiteral | Crystal::ProcLiteral,
+    ) : Bool
+      @rule.test(@source, node, @stack > 0)
+
+      true
+    end
+
+    private def incr_stack(&) : Nil
+      @stack += 1
+      yield
+      @stack -= 1
+    end
+
+    private def swap_stack(& : Int32 -> Nil) : Nil
+      old_stack = @stack
+      @stack = 0
+      yield old_stack
+      @stack = old_stack
+    end
+  end
+end
