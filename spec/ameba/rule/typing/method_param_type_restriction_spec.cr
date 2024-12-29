b724ee0887d4f43574d66d416d73dbe298d55307
Margret Riegert <m.a.riegert@snet.net>
2024-12-29 08:02:42 -0500

    Apply suggestions from code review
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- spec/ameba/rule/typing/method_param_type_restriction_spec.cr
+++ spec/ameba/rule/typing/method_param_type_restriction_spec.cr
@@ -8,44 +8,27 @@ module Ameba::Rule::Typing
       def hello(a : String) : String
         "hello world" + a
       end
-
-      # This method is documented
-      def hello(a : Bool)
-      end
-
-      private def hello(a : String) : String
-        "hello world" + a
-      end
-
-      protected def hello(a : String) : String
-        "hello world" + a
-      end
       CRYSTAL
   end
 
   it "passes if a private method method param doesn't have a type restriction" do
     expect_no_issues subject, <<-CRYSTAL
-      class Greeter
-        private def hello(a)
-          "hello world" + a
-        end
+      private def hello(a)
+        "hello world" + a
       end
       CRYSTAL
   end
 
   it "passes if a protected method param doesn't have a type restriction" do
     expect_no_issues subject, <<-CRYSTAL
-      class Greeter
-        protected def hello(a)
-          "hello world" + a
-        end
+      protected def hello(a)
+        "hello world" + a
       end
       CRYSTAL
   end
 
   it "fails if a public method param doesn't have a type restriction" do
     expect_issue subject, <<-CRYSTAL
-      # This is documentation about `hello`
       def hello(a)
               # ^ error: Method parameter should have a type restriction
         "hello world" + a
@@ -95,13 +78,11 @@ module Ameba::Rule::Typing
 
       it "fails if a public or private method doesn't have a return type restriction" do
         expect_issue rule, <<-CRYSTAL
-          # This method is documented
           def hello(a)
                   # ^ error: Method parameter should have a type restriction
             "hello world"
           end
 
-          # This method is also documented
           private def hello(a)
                           # ^ error: Method parameter should have a type restriction
             "hello world"
@@ -132,13 +113,11 @@ module Ameba::Rule::Typing
 
       it "fails if a public or protected method doesn't have a return type restriction" do
         expect_issue rule, <<-CRYSTAL
-          # This method is documented
           def hello(a)
                   # ^ error: Method parameter should have a type restriction
             "hello world"
           end
 
-          # This method is also documented
           protected def hello(a)
                             # ^ error: Method parameter should have a type restriction
             "hello world"
@@ -153,12 +132,9 @@ module Ameba::Rule::Typing
         rule.default_value = true
 
         expect_issue rule, <<-CRYSTAL
-          class Greeter
-            # This method is documented
-            def hello(a = "world")
-                    # ^ error: Method parameter should have a type restriction
-              "hello \#{a}"
-            end
+          def hello(a = "world")
+                  # ^ error: Method parameter should have a type restriction
+            "hello #{a}"
           end
           CRYSTAL
       end
