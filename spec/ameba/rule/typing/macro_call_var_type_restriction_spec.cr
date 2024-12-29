597d59a66c4469407eb0b9adc39604ac8ba03f7f
Margret Riegert <margret@eowyn.net>
2024-12-29 13:33:06 -0500

    Fix specs

--- spec/ameba/rule/typing/macro_call_var_type_restriction_spec.cr
+++ spec/ameba/rule/typing/macro_call_var_type_restriction_spec.cr
@@ -2,72 +2,73 @@ require "../../../spec_helper"
 
 module Ameba::Rule::Typing
   describe MacroCallVarTypeRestriction do
-  subject = MacroCallVarTypeRestriction.new
+    subject = MacroCallVarTypeRestriction.new
 
-  it "passes if macro call args have type restrictions" do
-    expect_no_issues subject, <<-CRYSTAL
-      class Greeter
-        getter name : String?
-        class_getter age : Int32 = 0
-        setter tasks : Array(String) = [] of String
-        class_setter queue : Array(Int32)?
-        property task_mutex : Mutex = Mutex.new
-        class_property asdf : String
-
-        record Task,
-          var1 : String,
-          var2 : String = "asdf"
-      end
-      CRYSTAL
-  end
-
-  it "fails if a macro call arg doesn't have a type restriction" do
-    expect_issue subject, <<-CRYSTAL
-      class Greeter
-        getter name
-             # ^^^^ error: Argument should have a type restriction
-      end
-      CRYSTAL
-  end
-
-  it "fails if a record call arg doesn't have a type restriction" do
-    expect_issue subject, <<-CRYSTAL
-      class Greeter
-        record Task,
-          var1 : String,
-          var2 = "asdf"
-        # ^^^^ error: Argument should have a type restriction
-      end
-      CRYSTAL
-  end
-
-  it "fails if a top-level record call arg doesn't have a type restriction" do
-    expect_issue subject, <<-CRYSTAL
-      record Task,
-        var1 : String,
-        var2 = "asdf"
-      # ^^^^ error: Argument should have a type restriction
-      CRYSTAL
-  end
-
-  context "#default_value" do
-    rule = MacroCallVarTypeRestriction.new
-    rule.default_value = false
-
-    it "passes if a macro call arg with a default value doesn't have a type restriction" do
-      expect_no_issues rule, <<-CRYSTAL
+    it "passes if macro call args have type restrictions" do
+      expect_no_issues subject, <<-CRYSTAL
         class Greeter
-          getter name = "Kenobi"
+          getter name : String?
+          class_getter age : Int32 = 0
+          setter tasks : Array(String) = [] of String
+          class_setter queue : Array(Int32)?
+          property task_mutex : Mutex = Mutex.new
+          class_property asdf : String
+
+          record Task,
+            var1 : String,
+            var2 : String = "asdf"
         end
         CRYSTAL
     end
 
-    it "passes if a top-level record call arg with default value doesn't have a type restriction" do
-      expect_no_issues rule, <<-CRYSTAL
+    it "fails if a macro call arg doesn't have a type restriction" do
+      expect_issue subject, <<-CRYSTAL
+        class Greeter
+          getter name
+               # ^^^^ error: Argument should have a type restriction
+        end
+        CRYSTAL
+    end
+
+    it "fails if a record call arg doesn't have a type restriction" do
+      expect_issue subject, <<-CRYSTAL
+        class Greeter
+          record Task,
+            var1 : String,
+            var2 = "asdf"
+          # ^^^^ error: Argument should have a type restriction
+        end
+        CRYSTAL
+    end
+
+    it "fails if a top-level record call arg doesn't have a type restriction" do
+      expect_issue subject, <<-CRYSTAL
         record Task,
           var1 : String,
           var2 = "asdf"
+        # ^^^^ error: Argument should have a type restriction
         CRYSTAL
+    end
+
+    context "#default_value" do
+      rule = MacroCallVarTypeRestriction.new
+      rule.default_value = false
+
+      it "passes if a macro call arg with a default value doesn't have a type restriction" do
+        expect_no_issues rule, <<-CRYSTAL
+          class Greeter
+            getter name = "Kenobi"
+          end
+          CRYSTAL
+      end
+
+      it "passes if a top-level record call arg with default value doesn't have a type restriction" do
+        expect_no_issues rule, <<-CRYSTAL
+          record Task,
+            var1 : String,
+            var2 = "asdf"
+          CRYSTAL
+      end
     end
   end
 end
