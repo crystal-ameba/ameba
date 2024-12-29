6a70b09f2927519d86472e71bbd3cc41061dcbc4
Sijawusz Pur Rahnama <sija@sija.pl>
2024-12-29 15:04:23 +0100

    Fix code indentation in specs

--- spec/ameba/rule/lint/trailing_rescue_exception_spec.cr
+++ spec/ameba/rule/lint/trailing_rescue_exception_spec.cr
@@ -5,21 +5,21 @@ module Ameba::Rule::Lint
 
   it "passes for trailing rescue with literal values" do
     expect_no_issues subject, <<-CRYSTAL
-        puts "hello" rescue "world"
-        puts :meow rescue 1234
-        CRYSTAL
+      puts "hello" rescue "world"
+      puts :meow rescue 1234
+      CRYSTAL
   end
 
   it "passes for trailing rescue with class initialization" do
     expect_no_issues subject, <<-CRYSTAL
-        puts "hello" rescue MyClass.new
-        CRYSTAL
+      puts "hello" rescue MyClass.new
+      CRYSTAL
   end
 
   it "fails if trailing rescue has exception name" do
     expect_issue subject, <<-CRYSTAL
-        puts "hello" rescue MyException
-                          # ^^^^^^^^^^^ error: Trailing rescues with a path aren't allowed, use a block rescue instead to filter by exception type
-        CRYSTAL
+      puts "hello" rescue MyException
+                        # ^^^^^^^^^^^ error: Trailing rescues with a path aren't allowed, use a block rescue instead to filter by exception type
+      CRYSTAL
   end
 end
