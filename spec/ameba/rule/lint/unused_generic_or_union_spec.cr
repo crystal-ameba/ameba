66a8cc1a3605a53ee3c65fddb9257eb25f905336
Margret Riegert <margret@eowyn.net>
2025-01-20 13:04:40 -0500

    Update specs

--- spec/ameba/rule/lint/unused_generic_or_union_spec.cr
+++ spec/ameba/rule/lint/unused_generic_or_union_spec.cr
@@ -23,93 +23,55 @@ module Ameba::Rule::Lint
         CRYSTAL
     end
 
-    it "passes if paths are used for methods" do
+    it "passes if generics and unions are used for methods" do
       expect_no_issues subject, <<-CRYSTAL
-        def size(a : Float64) : Float64
-          0.1 + a
+        def size(a : Float64?) : Float64?
+          0.1.try(&.+(a))
         end
 
-        fun fun_name = FunName(a : Int32) : Int32
-          1234 + a
+        def append(a : Array(String)) : Array(String)
+          a << "hello"
         end
         CRYSTAL
     end
 
-    it "passes if paths are used for classes, modules, aliases, and annotations" do
-      expect_no_issues subject, <<-CRYSTAL
-        module MyModule(T)
-          struct MyStruct < MySuperStruct
-          end
-        end
-
-        @[MyAnnotation]
-        class MyClass < MySuperClass
-          include MyModule(Int23)
-          extend MyModule(String)
-
-          alias MyType = Float64 | StaticArray(Float64, 10)
-        end
-
-        annotation MyAnnotation
-        end
-        CRYSTAL
-    end
-
-    it "passes if paths are used for lib objects" do
-      expect_no_issues subject, <<-CRYSTAL
-        lib MyLib
-          $external_var = ExternalVarName : Int32
-
-          type MyType = Void*
-
-          struct MyStruct
-            field1, field2 : Float64
-          end
-
-          fun fun_name = FunName(Void*) : Void
-        end
-        CRYSTAL
-    end
-
-    it "fails if paths and generic types are unused top-level" do
+    it "fails if generics or unions are unused top-level" do
       expect_issue subject, <<-CRYSTAL
-        Int32
-        # ^^^ error: Type or constant is not used
         String?
-        # ^^^^^ error: Type or constant is not used
+        # ^^^^^ error: Generic or union is not used
         Int32 | Float64 | Nil
-        # ^^^^^^^^^^^^^^^^^^^ error: Type or constant is not used
+        # ^^^^^^^^^^^^^^^^^^^ error: Generic or union is not used
         StaticArray(Int32, 10)
-        # ^^^^^^^^^^^^^^^^^^^^ error: Type or constant is not used
+        # ^^^^^^^^^^^^^^^^^^^^ error: Generic or union is not used
         CRYSTAL
     end
 
-    it "fails if types and constants are unused inside methods" do
+    it "fails if generics or unions are unused inside methods" do
       expect_issue subject, <<-CRYSTAL
         def hello
-          Float64
-        # ^^^^^^^ error: Type or constant is not used
+          Float64?
+        # ^^^^^^^^ error: Generic or union is not used
           0.1
         end
 
         fun fun_name : Int32
-          Int32
-        # ^^^^^ error: Type or constant is not used
+          Array(String)
+        # ^^^^^^^^^^^^^ error: Generic or union is not used
           1234
         end
         CRYSTAL
     end
 
-    it "fails if types and constants are unused inside classes and modules" do
+    it "fails if generics or unions are unused inside classes and modules" do
       expect_issue subject, <<-CRYSTAL
         class MyClass
-          MyModule
-        # ^^^^^^^^ error: Type or constant is not used
+          String?
+        # ^^^^^^^ error: Generic or union is not used
         end
 
         module MyModule
-          Int32
-        # ^^^^^ error: Type or constant is not used
+          Array(Int32)
+        # ^^^^^^^^^^^^ error: Generic or union is not used
         end
         CRYSTAL
     end
