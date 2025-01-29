06d3e9d22abba75a9de00c0476fcaaff443c9f12
Margret Riegert <margret@eowyn.net>
2025-01-29 10:34:28 -0500

    Add `Lint/UnusedLocalVariableAccess`

---
+++ spec/ameba/rule/lint/unused_local_variable_access_spec.cr
@@ -0,0 +1,46 @@
+require "../../../spec_helper"
+
+module Ameba::Rule::Lint
+  subject = UnusedLocalVariableAccess.new
+
+  describe UnusedLocalVariableAccess do
+    it "passes if local variables are used" do
+      expect_no_issues subject, <<-CRYSTAL
+        a = 1
+
+        puts a
+
+        a += 1
+        a, b = 2, 3
+        CRYSTAL
+    end
+
+    it "passes if local variable on left side of a comparison" do
+      expect_no_issues subject, <<-CRYSTAL
+        def hello
+          a = 1
+          a || (puts "a is falsey")
+          a
+        end
+        CRYSTAL
+    end
+
+    it "passes if skip_file exists in a macro" do
+      expect_no_issues subject, <<-CRYSTAL
+        {% skip_file %}
+        CRYSTAL
+    end
+
+    it "fails if local variables are unused" do
+      expect_issue subject, <<-CRYSTAL
+        a = 1
+
+        begin
+          a
+        # ^ error: Value from local variable access is unused
+          puts a
+        end
+        CRYSTAL
+    end
+  end
+end
