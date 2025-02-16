d28076c0fb6e0280e4d66c9e4f3cf5990e4f49c9
Sijawusz Pur Rahnama <sija@sija.pl>
2025-02-16 14:48:12 +0100

    Add `Lint/SpecEqWithBoolOrNilLiteral` rule (#567)

---
+++ spec/ameba/rule/lint/spec_eq_with_bool_or_nil_literal_spec.cr
@@ -0,0 +1,53 @@
+require "../../../spec_helper"
+
+module Ameba::Rule::Lint
+  describe SpecEqWithBoolOrNilLiteral do
+    subject = SpecEqWithBoolOrNilLiteral.new
+
+    it "does not report `eq` method calls that are not arguments to `should`" do
+      expect_no_issues subject, <<-CRYSTAL, path: "source_spec.cr"
+        foo.bar? eq true
+        foo.bar? eq false
+        foo.bar? eq nil
+        CRYSTAL
+    end
+
+    it "does not report if `be_true` / `be_false` expectation is used" do
+      expect_no_issues subject, <<-CRYSTAL, path: "source_spec.cr"
+        foo.is_a?(String).should be_true
+        foo.is_a?(Int32).should be_false
+        CRYSTAL
+    end
+
+    it "does not report if `be_nil` expectation is used" do
+      expect_no_issues subject, <<-CRYSTAL, path: "source_spec.cr"
+        foo.as?(Symbol).should be_nil
+        CRYSTAL
+    end
+
+    it "reports if `eq` expectation with bool literal is used" do
+      source = expect_issue subject, <<-CRYSTAL, path: "source_spec.cr"
+        foo.is_a?(String).should eq true
+                               # ^^^^^^^ error: Use `be_true` instead of `eq(true)` expectation
+        foo.is_a?(Int32).should eq false
+                              # ^^^^^^^^ error: Use `be_false` instead of `eq(false)` expectation
+        CRYSTAL
+
+      expect_correction source, <<-CRYSTAL
+        foo.is_a?(String).should be_true
+        foo.is_a?(Int32).should be_false
+        CRYSTAL
+    end
+
+    it "reports if `eq` expectation with nil literal is used" do
+      source = expect_issue subject, <<-CRYSTAL, path: "source_spec.cr"
+        foo.as?(Symbol).should eq nil
+                             # ^^^^^^ error: Use `be_nil` instead of `eq(nil)` expectation
+        CRYSTAL
+
+      expect_correction source, <<-CRYSTAL
+        foo.as?(Symbol).should be_nil
+        CRYSTAL
+    end
+  end
+end
---
+++ src/ameba/rule/lint/spec_eq_with_bool_or_nil_literal.cr
@@ -0,0 +1,62 @@
+module Ameba::Rule::Lint
+  # Reports `eq(true|false|nil)` expectations in specs.
+  #
+  # This is considered bad:
+  #
+  # ```
+  # it "works" do
+  #   foo.is_a?(String).should eq true
+  #   foo.is_a?(Int32).should eq false
+  #   foo.as?(Symbol).should eq nil
+  # end
+  # ```
+  #
+  # And it should be written as the following:
+  #
+  # ```
+  # it "works" do
+  #   foo.is_a?(String).should be_true
+  #   foo.is_a?(Int32).should be_false
+  #   foo.as?(Symbol).should be_nil
+  # end
+  # ```
+  #
+  # YAML configuration example:
+  #
+  # ```
+  # Lint/SpecEqWithBoolOrNilLiteral:
+  #   Enabled: true
+  # ```
+  class SpecEqWithBoolOrNilLiteral < Base
+    properties do
+      since_version "1.7.0"
+      description "Reports `eq(true|false|nil)` expectations in specs"
+    end
+
+    MSG = "Use `%s` instead of `%s` expectation"
+
+    def test(source)
+      return super if source.spec?
+    end
+
+    def test(source, node : Crystal::Call)
+      return unless node.name == "should"
+      return unless node.block.nil? && node.args.size == 1
+
+      return unless (matcher = node.args.first).is_a?(Crystal::Call)
+      return unless matcher.name == "eq"
+      return unless matcher.block.nil? && matcher.args.size == 1
+
+      replacement =
+        case arg = matcher.args.first
+        when Crystal::BoolLiteral then arg.value ? "be_true" : "be_false"
+        when Crystal::NilLiteral  then "be_nil"
+        end
+      return unless replacement
+
+      issue_for matcher, MSG % {replacement, matcher.to_s} do |corrector|
+        corrector.replace(matcher, replacement)
+      end
+    end
+  end
+end
