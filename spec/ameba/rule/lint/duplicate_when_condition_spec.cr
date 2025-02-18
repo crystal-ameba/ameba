552bba4f843ef6a8c82a751794ff1f80a2d40167
Sijawusz Pur Rahnama <sija@sija.pl>
2025-02-18 18:43:42 +0100

    Add support for `select` control expressions in `Lint/DuplicateWhenCondition` rule

--- spec/ameba/rule/lint/duplicate_when_condition_spec.cr
+++ spec/ameba/rule/lint/duplicate_when_condition_spec.cr
@@ -15,7 +15,7 @@ module Ameba::Rule::Lint
         CRYSTAL
     end
 
-    it "reports if there are a duplicated `when` conditions" do
+    it "reports if there are a duplicated `when` conditions in `case` expression" do
       expect_issue subject, <<-CRYSTAL
         case x
         when .foo?, .nil?
@@ -38,6 +38,20 @@ module Ameba::Rule::Lint
         when Time.utc.year == 1996
            # ^^^^^^^^^^^^^^^^^^^^^ error: Duplicate `when` condition detected
           :yo
+        end
+        CRYSTAL
+    end
+
+    it "reports if there are a duplicated `when` conditions in `select` expression" do
+      expect_issue subject, <<-CRYSTAL
+        select
+        when foo = foo_channel.receive
+          puts foo
+        when foo = foo_channel.receive
+           # ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Duplicate `when` condition detected
+          puts foo
+        when bar = bar_channel.receive?
+          puts bar
         end
         CRYSTAL
     end
--- src/ameba/rule/lint/duplicate_when_condition.cr
+++ src/ameba/rule/lint/duplicate_when_condition.cr
@@ -37,7 +37,7 @@ module Ameba::Rule::Lint
 
     MSG = "Duplicate `when` condition detected"
 
-    def test(source, node : Crystal::Case)
+    def test(source, node : Crystal::Case | Crystal::Select)
       node.whens.each_with_object(Set(String).new) do |when_node, processed_conditions|
         when_node.conds.each do |cond|
           cond_s = cond.to_s
