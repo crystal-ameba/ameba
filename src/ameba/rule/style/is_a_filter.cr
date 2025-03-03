186cb69b30e5f47f5f3ada2fa633076dd0b2ade6
Sijawusz Pur Rahnama <sija@sija.pl>
2025-03-03 14:53:04 +0100

    Cleanup `Style/IsAFilter` rule to avoid triggering cyclomatic complexity issue

--- src/ameba/rule/style/is_a_filter.cr
+++ src/ameba/rule/style/is_a_filter.cr
@@ -58,33 +58,25 @@ module Ameba::Rule::Style
 
     def test(source, node : Crystal::Call)
       return unless node.name.in?(filter_names)
-      return unless filter_location = name_location(node)
-      return unless block = node.block
+      return unless location = name_location(node)
+      return unless (block = node.block) && block.args.size == 1
       return unless (body = block.body).is_a?(Crystal::IsA)
       return unless (path = body.const).is_a?(Crystal::Path)
       return unless body.obj.is_a?(Crystal::Var)
-      return if block.args.size > 1
 
       name = path.names.join("::")
       name = "::#{name}" if path.global? && !body.nil_check?
-
-      end_location = node.end_location
-      if !end_location || end_location.try(&.column_number.zero?)
-        if end_location = path.end_location
-          end_location = end_location.adjust(column_number: 1)
-        end
-      end
 
       old = OLD % node.name
       new = NEW % {node.name, name}
       msg = MSG % {new, old}
 
-      if end_location
-        issue_for(filter_location, end_location, msg) do |corrector|
-          corrector.replace(filter_location, end_location, new)
+      if end_location = node.end_location
+        issue_for(location, end_location, msg) do |corrector|
+          corrector.replace(location, end_location, new)
         end
       else
-        issue_for(filter_location, nil, msg)
+        issue_for(location, nil, msg)
       end
     end
   end
