858ac3af5944dc207843abccd5a74858436c1d0c
Sijawusz Pur Rahnama <sija@sija.pl>
2025-03-04 06:39:07 +0100

    Fix config sample fixture path resolution

--- spec/ameba/config_spec.cr
+++ spec/ameba/config_spec.cr
@@ -2,7 +2,7 @@ require "../spec_helper"
 
 module Ameba
   describe Config do
-    config_sample = Path["spec", "fixtures", "config.yml"].to_s
+    config_sample = Path[__DIR__, "..", "fixtures", "config.yml"].to_s
 
     it "should have a list of available formatters" do
       Config::AVAILABLE_FORMATTERS.should_not be_nil
