2b48dd1f4a263849840e068dad1e024b380e5eb5
Johannes Müller <straightshoota@gmail.com>
2025-06-10 16:13:10 +0200

    Update src/ameba/ast/scope.cr
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/ameba/ast/scope.cr
+++ src/ameba/ast/scope.cr
@@ -145,7 +145,7 @@ module Ameba::AST
     # Returns `true` if type declaration variable is assigned in this scope.
     def assigns_type_dec?(name)
       type_dec_variables.any?(&.name.== name) ||
-        inherited? { !!outer_scope.try(&.assigns_type_dec?(name)) }
+        !!inherited? { outer_scope.try(&.assigns_type_dec?(name)) }
     end
 
     # Returns `true` if and only if current scope represents some
