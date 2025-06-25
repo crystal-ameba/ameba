6ccbcf17a486123e3617ddc9431b05e08040b2a8
Margret Riegert <margret@eowyn.net>
2025-06-25 10:29:37 -0400

    Updates from review

--- src/ameba/rule/lint/unused_expression.cr
+++ src/ameba/rule/lint/unused_expression.cr
@@ -45,28 +45,32 @@ module Ameba::Rule::Lint
       description "Disallows unused expressions"
     end
 
+    COMPARISON_OPERATORS = %w[== != < <= > >= <=>]
+
+    MSG_CLASS_VAR     = "Value from class variable access is unused"
+    MSG_COMPARISON    = "Comparison operation is unused"
+    MSG_GENERIC       = "Generic is not used"
+    MSG_UNION         = "Union is not used"
+    MSG_INSTANCE_VAR  = "Value from instance variable access is unused"
+    MSG_LITERAL       = "Literal value is not used"
+    MSG_LOCAL_VAR     = "Value from local variable access is unused"
+    MSG_SELF          = "`self` is not used"
+    MSG_PSEUDO_METHOD = "Pseudo-method call is not used"
+
     def test(source : Source)
       AST::ImplicitReturnVisitor.new(self, source)
     end
-
-    CLASS_VAR_MSG = "Value from class variable access is unused"
 
     def test(source, node : Crystal::ClassVar, in_macro : Bool)
       # Class variables aren't supported in macros
       return if in_macro
 
-      issue_for node, CLASS_VAR_MSG
+      issue_for node, MSG_CLASS_VAR
     end
-
-    COMPARISON_MSG       = "Comparison operation is unused"
-    COMPARISON_OPERATORS = %w[== != < <= > >= <=>]
-
-    MSG_GENERIC = "Generic is not used"
-    MSG_UNION   = "Union is not used"
 
     def test(source, node : Crystal::Call, in_macro : Bool)
       if node.name.in?(COMPARISON_OPERATORS) && node.args.size == 1
-        issue_for node, COMPARISON_MSG
+        issue_for node, MSG_COMPARISON
       end
 
       if path_or_generic_union?(node)
@@ -76,6 +80,54 @@ module Ameba::Rule::Lint
 
     def test(source, node : Crystal::Generic, in_macro : Bool)
       issue_for node, MSG_GENERIC
+    end
+
+    def test(source, node : Crystal::InstanceVar, in_macro : Bool)
+      # Handle special case when using `@type` within a method body has side-effects
+      return if in_macro && node.name == "@type"
+
+      issue_for node, MSG_INSTANCE_VAR
+    end
+
+    def test(source, node : Crystal::RegexLiteral, in_macro : Bool)
+      # Locations for Regex literals were added in Crystal v1.15.0
+      {% if compare_versions(Crystal::VERSION, "1.15.0") >= 0 %}
+        issue_for node, MSG_LITERAL
+      {% end %}
+    end
+
+    def test(
+      source,
+      node : Crystal::BoolLiteral | Crystal::CharLiteral | Crystal::HashLiteral |
+             Crystal::ProcLiteral | Crystal::ArrayLiteral | Crystal::RangeLiteral |
+             Crystal::TupleLiteral | Crystal::NumberLiteral |
+             Crystal::StringLiteral | Crystal::SymbolLiteral |
+             Crystal::NamedTupleLiteral | Crystal::StringInterpolation,
+      in_macro : Bool,
+    )
+      issue_for node, MSG_LITERAL
+    end
+
+    def test(source, node : Crystal::Var, in_macro : Bool)
+      if node.name == "self"
+        issue_for node, MSG_SELF
+        return
+      end
+
+      # Ignore `debug` and `skip_file` macro methods
+      return if in_macro && node.name.in?("debug", "skip_file")
+
+      issue_for node, MSG_LOCAL_VAR
+    end
+
+    def test(
+      source,
+      node : Crystal::PointerOf | Crystal::SizeOf | Crystal::InstanceSizeOf |
+             Crystal::AlignOf | Crystal::InstanceAlignOf | Crystal::OffsetOf |
+             Crystal::IsA | Crystal::NilableCast | Crystal::RespondsTo | Crystal::Not,
+      in_macro : Bool,
+    )
+      issue_for node, MSG_PSEUDO_METHOD
     end
 
     private def path_or_generic_union?(node : Crystal::Call) : Bool
@@ -94,64 +146,6 @@ module Ameba::Rule::Lint
       else
         false
       end
-    end
-
-    INSTANCE_VAR_MSG = "Value from instance variable access is unused"
-
-    def test(source, node : Crystal::InstanceVar, in_macro : Bool)
-      # Handle special case when using `@type` within a method body has side-effects
-      return if in_macro && node.name == "@type"
-
-      issue_for node, INSTANCE_VAR_MSG
-    end
-
-    LITERAL_MSG = "Literal value is not used"
-
-    def test(source, node : Crystal::RegexLiteral, in_macro : Bool)
-      # Locations for Regex literals were added in Crystal v1.15.0
-      {% if compare_versions(Crystal::VERSION, "1.15.0") >= 0 %}
-        issue_for node, LITERAL_MSG
-      {% end %}
-    end
-
-    def test(
-      source,
-      node : Crystal::BoolLiteral | Crystal::CharLiteral | Crystal::HashLiteral |
-             Crystal::ProcLiteral | Crystal::ArrayLiteral | Crystal::RangeLiteral |
-             Crystal::TupleLiteral | Crystal::NumberLiteral |
-             Crystal::StringLiteral | Crystal::SymbolLiteral |
-             Crystal::NamedTupleLiteral | Crystal::StringInterpolation,
-      in_macro : Bool,
-    )
-      issue_for node, LITERAL_MSG
-    end
-
-    LOCAL_VAR_MSG = "Value from local variable access is unused"
-
-    SELF_MSG = "`self` is not used"
-
-    def test(source, node : Crystal::Var, in_macro : Bool)
-      if node.name == "self"
-        issue_for node, SELF_MSG
-        return
-      end
-
-      # Ignore `debug` and `skip_file` macro methods
-      return if in_macro && node.name.in?("debug", "skip_file")
-
-      issue_for node, LOCAL_VAR_MSG
-    end
-
-    PSEUDO_METHOD_MSG = "Pseudo-method call is not used"
-
-    def test(
-      source,
-      node : Crystal::PointerOf | Crystal::SizeOf | Crystal::InstanceSizeOf |
-             Crystal::AlignOf | Crystal::InstanceAlignOf | Crystal::OffsetOf |
-             Crystal::IsA | Crystal::NilableCast | Crystal::RespondsTo | Crystal::Not,
-      in_macro : Bool,
-    )
-      issue_for node, PSEUDO_METHOD_MSG
     end
   end
 end
