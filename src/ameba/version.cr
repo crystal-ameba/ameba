7dc3c1619730c87f1748d4bce1b97876ab92ba94
Pedro Borges <83559896+SunP04@users.noreply.github.com>
2025-10-17 09:16:34 -0300

    Fix Git SHA command for windows (#667)
    * feat: added conditional compilation for windows for gitsha
    
    * sug: remove git sha for windows
    
    Suggestion made by Sija:
    
    https://github.com/crystal-ameba/ameba/pull/667#discussion_r2421643265
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>
    
    ---------
    
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/ameba/version.cr
+++ src/ameba/version.cr
@@ -1,4 +1,8 @@
 module Ameba
   VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
-  GIT_SHA = {{ `(git rev-parse --short HEAD || true) 2>/dev/null`.chomp.stringify }}.presence
+  {% if flag?(:windows) %}
+    GIT_SHA = nil
+  {% else %}
+    GIT_SHA = {{ `(git rev-parse --short HEAD || true) 2>/dev/null`.chomp.stringify }}.presence
+  {% end %}
 end
