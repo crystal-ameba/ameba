5a98451492fe63af406fd20e858568806287f86b
Sijawusz Pur Rahnama <sija@sija.pl>
2025-11-12 01:22:44 +0100

    More JSON Schema generator refactors

--- src/schema.cr
+++ src/schema.cr
@@ -1,102 +1,123 @@
 require "./ameba"
 
-schema = JSON.build(2) do |builder|
-  builder.object do
-    builder.field("$schema", "http://json-schema.org/draft-07/schema#")
-    builder.field("title", ".ameba.yml")
-    builder.field("description", "Configuration rules for the Crystal language Ameba linter")
-    builder.field("type", "object")
-    builder.field("additionalProperties", false)
+JSON_SCHEMA_FILEPATH =
+  Path[".ameba.yml.schema.json"]
 
-    builder.string("definitions")
-    builder.object do
-      builder.string("Severity")
+module Ameba::JSONSchema
+  extend self
+
+  def build(indent = 2) : String
+    String.build do |str|
+      build(str, indent)
+    end
+  end
+
+  def build(path : Path, indent = 2) : Nil
+    File.open(path, "w") do |file|
+      build(file, indent)
+    end
+  end
+
+  def build(io : IO, indent = 2) : Nil
+    JSON.build(io, indent: indent) do |builder|
       builder.object do
-        builder.field("type", "string")
-        builder.string("enum")
-        builder.array do
-          Ameba::Severity.values.each do |value|
-            builder.string(value.to_s)
+        builder.field("$schema", "http://json-schema.org/draft-07/schema#")
+        builder.field("title", ".ameba.yml")
+        builder.field("description", "Configuration rules for the Crystal language Ameba linter")
+        builder.field("type", "object")
+        builder.field("additionalProperties", false)
+
+        builder.string("definitions")
+        builder.object do
+          builder.string("Severity")
+          builder.object do
+            builder.field("type", "string")
+            builder.string("enum")
+            builder.array do
+              Severity.values.each do |value|
+                builder.string(value.to_s)
+              end
+            end
+          end
+
+          builder.string("BaseRule")
+          builder.object do
+            builder.field("type", "object")
+
+            builder.string("properties")
+            builder.object do
+              builder.string("SinceVersion")
+              builder.object do
+                builder.field("type", "string")
+              end
+
+              builder.string("Enabled")
+              builder.object do
+                builder.field("type", "boolean")
+                builder.field("default", true)
+              end
+
+              builder.string("Severity")
+              builder.object do
+                builder.field("$ref", "#/definitions/Severity")
+                builder.field("default", Rule::Base.default_severity.to_s)
+              end
+
+              builder.string("Excluded")
+              builder.object do
+                builder.field("type", "array")
+
+                builder.string("items")
+                builder.object do
+                  builder.field("type", "string")
+                end
+              end
+            end
           end
         end
-      end
-
-      builder.string("BaseRule")
-      builder.object do
-        builder.field("type", "object")
 
         builder.string("properties")
         builder.object do
-          builder.string("SinceVersion")
+          builder.string("Version")
           builder.object do
             builder.field("type", "string")
-          end
-
-          builder.string("Enabled")
-          builder.object do
-            builder.field("type", "boolean")
-            builder.field("default", true)
-          end
-
-          builder.string("Severity")
-          builder.object do
-            builder.field("$ref", "#/definitions/Severity")
-            builder.field("default", Ameba::Rule::Base.default_severity.to_s)
+            builder.field("description",
+              "The version of Ameba to limit rules to")
           end
 
           builder.string("Excluded")
           builder.object do
             builder.field("type", "array")
+            builder.field("title", "Excluded files and paths")
+            builder.field("description",
+              "An array of wildcards (or paths) to exclude from the source list")
 
             builder.string("items")
             builder.object do
               builder.field("type", "string")
             end
           end
+
+          builder.string("Globs")
+          builder.object do
+            builder.field("type", "array")
+            builder.field("title", "Globbed files and paths")
+            builder.field("description",
+              "An array of wildcards (or paths) to include to the inspection")
+
+            builder.string("items")
+            builder.object do
+              builder.field("type", "string")
+            end
+          end
+
+          Rule.rules.each do |rule|
+            rule.to_json_schema(builder)
+          end
         end
-      end
-    end
-
-    builder.string("properties")
-    builder.object do
-      builder.string("Version")
-      builder.object do
-        builder.field("type", "string")
-        builder.field("description",
-          "The version of Ameba to limit rules to")
-      end
-
-      builder.string("Excluded")
-      builder.object do
-        builder.field("type", "array")
-        builder.field("title", "Excluded files and paths")
-        builder.field("description",
-          "An array of wildcards (or paths) to exclude from the source list")
-
-        builder.string("items")
-        builder.object do
-          builder.field("type", "string")
-        end
-      end
-
-      builder.string("Globs")
-      builder.object do
-        builder.field("type", "array")
-        builder.field("title", "Globbed files and paths")
-        builder.field("description",
-          "An array of wildcards (or paths) to include to the inspection")
-
-        builder.string("items")
-        builder.object do
-          builder.field("type", "string")
-        end
-      end
-
-      Ameba::Rule.rules.each do |rule|
-        rule.to_json_schema(builder)
       end
     end
   end
 end
 
-File.write(".ameba.yml.schema.json", schema)
+Ameba::JSONSchema.build(JSON_SCHEMA_FILEPATH)
