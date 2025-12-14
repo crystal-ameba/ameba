6e4c76e4b6cab9d633dd6eaa856bfcf11fa823a6
Sijawusz Pur Rahnama <sija@sija.pl>
2025-12-14 17:47:36 +0100

    Use absolute path for JSON Schema filepath

--- src/json-schema-builder.cr
+++ src/json-schema-builder.cr
@@ -2,6 +2,6 @@ require "./ameba"
 require "./ameba/json_schema/*"
 
 JSON_SCHEMA_FILEPATH =
-  Path[".ameba.yml.schema.json"]
+  Path[__DIR__, "..", ".ameba.yml.schema.json"].expand
 
 Ameba::JSONSchema::Builder.build(JSON_SCHEMA_FILEPATH)
