b5b8960c94c0ad7910c53fe8c41d92c5e4bb88e1
Sijawusz Pur Rahnama <sija@sija.pl>
2025-12-19 22:36:59 +0100

    Add spec covering issues sorting in `Runner`

--- spec/ameba/runner_spec.cr
+++ spec/ameba/runner_spec.cr
@@ -101,6 +101,23 @@ module Ameba
         end
       end
 
+      context "issues sorting" do
+        it "sorts issues by severity, line number, and column number" do
+          rules = [
+            ErrorRule.from_yaml("{ Severity: convention, Message: foo }"),
+            ErrorRule.from_yaml("{ Severity: convention, Message: bar, LineNumber: 2 }"),
+            ErrorRule.from_yaml("{ Severity: warning, Message: baz, LineNumber: 2 }"),
+            ErrorRule.from_yaml("{ Severity: error, Message: bat, LineNumber: 2 }"),
+            ErrorRule.from_yaml("{ Severity: warning, Message: baq }"),
+          ] of Rule::Base
+          source = Source.new "foo\nbar"
+
+          Runner.new(rules, [source], formatter, default_severity).run
+          source.should_not be_valid
+          source.issues.map(&.message).should eq %w[foo baq bar baz bat]
+        end
+      end
+
       context "invalid syntax" do
         it "reports a syntax error" do
           rules = [Rule::Lint::Syntax.new] of Rule::Base
