83cb2f466a9e152fba8ab4a342587cf4e6934677
Sijawusz Pur Rahnama <sija@sija.pl>
2026-01-02 04:03:20 +0100

    Reorder `require` sections in ameba.cr

--- bin/ameba.cr
+++ bin/ameba.cr
@@ -1,9 +1,9 @@
 #!/usr/bin/env crystal
 
-# Require ameba cli which starts the inspection.
-require "ameba/cli"
-
 # Require ameba extensions here which are added as project dependencies.
 # Example:
 #
 # require "ameba-performance"
+
+# Require ameba cli which starts the inspection.
+require "ameba/cli"
