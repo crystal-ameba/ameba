7b6806218b9d6f64d9f1236be59936d173d1edf4
Sijawusz Pur Rahnama <sija@sija.pl>
2026-04-08 01:17:22 +0200

    Do not report non-existent rules in `Lint/BadDirective` rule

--- spec/ameba/rule/lint/bad_directive_spec.cr
+++ spec/ameba/rule/lint/bad_directive_spec.cr
@@ -4,7 +4,7 @@ module Ameba::Rule::Lint
   describe BadDirective do
     subject = BadDirective.new
 
-    it "does not report if rule is correct" do
+    it "does not report if action is correct" do
       expect_no_issues subject, <<-CRYSTAL
         # ameba:disable Lint/BadDirective
         CRYSTAL
@@ -14,13 +14,6 @@ module Ameba::Rule::Lint
       expect_issue subject, <<-CRYSTAL
         # ameba:foo Lint/BadDirective
               # ^^^ error: Bad action in comment directive: `foo`. Possible values: `disable`, `enable`
-        CRYSTAL
-    end
-
-    it "reports if there are incorrect rule names" do
-      expect_issue subject, <<-CRYSTAL
-        # ameba:enable BadRule1, BadRule2
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Such rules do not exist: `BadRule1`, `BadRule2`
         CRYSTAL
     end
 
@@ -34,12 +27,6 @@ module Ameba::Rule::Lint
       expect_no_issues subject, <<-CRYSTAL
         # ameba:enable
         # ameba:disable
-        CRYSTAL
-    end
-
-    it "does not report if there are group names in the directive" do
-      expect_no_issues subject, <<-CRYSTAL
-        # ameba:disable Style Performance
         CRYSTAL
     end
   end
