dea15209089f9881b17e8fdc16a2cf47d64266a5
Sijawusz Pur Rahnama <sija@sija.pl>
2026-04-14 21:24:12 +0200

    Extract `AST::MacroReferenceFinder` into its own file

--- src/ameba/ast/variabling/variable.cr
+++ src/ameba/ast/variabling/variable.cr
@@ -145,41 +145,5 @@ module Ameba::AST
         (var_location.line_number == node_location.line_number &&
           var_location.column_number < node_location.column_number)
     end
-
-    private class MacroReferenceFinder < Crystal::Visitor
-      property? references = false
-
-      def initialize(node, @reference : String)
-        node.accept self
-      end
-
-      @[AlwaysInline]
-      private def includes_reference?(val)
-        val.to_s.includes?(@reference)
-      end
-
-      def visit(node : Crystal::MacroLiteral)
-        !(@references ||= includes_reference?(node.value))
-      end
-
-      def visit(node : Crystal::MacroExpression)
-        !(@references ||= includes_reference?(node.exp))
-      end
-
-      def visit(node : Crystal::MacroFor)
-        !(@references ||= includes_reference?(node.exp) ||
-                          includes_reference?(node.body))
-      end
-
-      def visit(node : Crystal::MacroIf)
-        !(@references ||= includes_reference?(node.cond) ||
-                          includes_reference?(node.then) ||
-                          includes_reference?(node.else))
-      end
-
-      def visit(node : Crystal::ASTNode)
-        true
-      end
-    end
   end
 end
