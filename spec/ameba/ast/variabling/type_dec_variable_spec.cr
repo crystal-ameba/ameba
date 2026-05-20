c408a3563a5beb73ec9c86fadcc37953933f2cde
Sijawusz Pur Rahnama <sija@sija.pl>
2026-05-20 04:05:17 +0200

    Add support for constants in `AST::TypeDecVariable#name`

--- spec/ameba/ast/variabling/type_dec_variable_spec.cr
+++ spec/ameba/ast/variabling/type_dec_variable_spec.cr
@@ -4,10 +4,11 @@ module Ameba::AST
   describe TypeDecVariable do
     var = Crystal::Var.new("foo")
     declared_type = Crystal::Path.new("String")
-    type_dec = Crystal::TypeDeclaration.new(var, declared_type)
 
     describe "#initialize" do
       it "creates a new type dec variable" do
+        type_dec = Crystal::TypeDeclaration.new(var, declared_type)
+
         variable = TypeDecVariable.new(type_dec)
         variable.node.should_not be_nil
       end
@@ -15,14 +16,25 @@ module Ameba::AST
 
     describe "#name" do
       it "returns var name" do
+        type_dec = Crystal::TypeDeclaration.new(var, declared_type)
+
         variable = TypeDecVariable.new(type_dec)
         variable.name.should eq var.name
       end
 
-      it "raises if type declaration is incorrect" do
-        type_dec = Crystal::TypeDeclaration.new(declared_type, declared_type)
+      it "returns const name" do
+        const = Crystal::Path.new(%w[Foo Bar])
+        type_dec = Crystal::TypeDeclaration.new(const, declared_type)
 
-        expect_raises(Exception, "Unsupported var node type: Crystal::Path") do
+        variable = TypeDecVariable.new(type_dec)
+        variable.name.should eq const.names.join("::")
+      end
+
+      it "raises if type declaration is incorrect" do
+        union = Crystal::Union.new([declared_type, declared_type] of Crystal::ASTNode)
+        type_dec = Crystal::TypeDeclaration.new(union, declared_type)
+
+        expect_raises(Exception, "Unsupported var node type: Crystal::Union") do
           TypeDecVariable.new(type_dec).name
         end
       end
